7e4cab3f01629b2676790c5ecfea640f806159d9
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-11-07 20:36:25 +0700

    add example get for single record

--- example/get/single_record/get.go
+++ example/get/single_record/get.go
@@ -4,158 +4,142 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	pkg "github.com/megadata-dev/routerosv7-restfull-api"
+	"github.com/megadata-dev/routerosv7-restfull-api"
 	"strings"
 )
 
-// Create constants for the default host, username, and password.
+// Create constants for the default values for this example application
 const (
-	defaultHost     = "10.90.0.251" // defaultHost is the default host for the RouterOS device
-	defaultUsername = "userapi"     // defaultUsername is the default username for the RouterOS device
-	defaultPassword = "password"    // defaultPassword is the default password for the RouterOS device
+	defaultHost     = "10.90.0.251" // Change this to the IP address of your RouterOS device
+	defaultUsername = "userapi"     // Change this to the username of your RouterOS device
+	defaultPassword = "password"    // Change this to the password of your RouterOS device
 )
 
-// AppConfig is a struct that contains the configuration for the application.
+// AppConfig struct for this example application configuration values
 type AppConfig struct {
-	Host     string            // Host is the IP address of the RouterOS device
-	Username string            // Username is the username for the RouterOS device
-	Password string            // Password is the password for the RouterOS device
-	Command  string            // Command is the command to be executed
-	Params   map[string]string //Params is the parameter for the command
+	Host     string            // IP address of the RouterOS device
+	Username string            // Username of the RouterOS device
+	Password string            // Password of the RouterOS device
+	Command  string            // Command to execute
+	Params   map[string]string // Params for the command
 }
 
-/*
-NewAppConfig is a function to create a new AppConfig.
-It accepts the host, username, password, and command as parameters.
-It returns a pointer to an AppConfig.
-*/
+// NewAppConfig function to create new AppConfig instance with default values for this example application
 func NewAppConfig(host, username, password, command string, params map[string]string) *AppConfig {
 	return &AppConfig{
-		Host:     host,     // Set the host
-		Username: username, // Set the username
-		Password: password, // Set the password
-		Command:  command,  // Set the command
-		Params:   params,   // Set the params
+		Host:     host,     // IP address of the RouterOS device
+		Username: username, // Username of the RouterOS device
+		Password: password, // Password of the RouterOS device
+		Command:  command,  // Command to execute
+		Params:   params,   // Params for the command
 	}
 }
 
-/*
-DataRetriever is an interface that contains the GetData method.
-This method will be used to retrieve data from the RouterOS device.
-The GetData method accepts a context.Context as a parameter and returns an interface and an error.
-*/
+// DataRetriever interface for retrieving data from RouterOS device
 type DataRetriever interface {
 	GetData(ctx context.Context) (interface{}, error)
 }
 
-// RouterOSDataRetriever is a struct that implements the DataRetriever interface.
+// RouterOSDataRetriever struct for retrieving data from RouterOS device
 type RouterOSDataRetriever struct {
-	config *AppConfig // config is a pointer to an AppConfig
+	config *AppConfig
 }
 
-/*
-NewRouterOSDataRetriever is a function to create a new RouterOSDataRetriever.
-It accepts an AppConfig as a parameter and returns a DataRetriever.
-The function returns a pointer to a RouterOSDataRetriever.
-*/
+// NewRouterOSDataRetriever function to create new RouterOSDataRetriever instance
 func NewRouterOSDataRetriever(config *AppConfig) DataRetriever {
 	return &RouterOSDataRetriever{config: config}
 }
 
-/*
-GetData is a method to retrieve data from the RouterOS device.
-for Example i want request with single record with multiple params in Mikrotik RouterOS
-you can use this function to get data from Mikrotik RouterOS
-*/
+// GetData function to retrieve data from RouterOS device using the config values from the RouterOSDataRetriever
 func (r *RouterOSDataRetriever) GetData(ctx context.Context) (interface{}, error) {
 
-	// Create a slice of strings to store the query parameters
+	// Create queryParams variable as slice of string
 	var queryParams []string
 
-	// Loop through the params and append them to the slice of strings as key=value
+	// Loop through the params and append the key and value to the queryParams slice
 	for key, value := range r.config.Params {
-		queryParams = append(queryParams, fmt.Sprintf("%s=%s", key, value)) // Set the query parameters
+		queryParams = append(queryParams, fmt.Sprintf("%s=%s", key, value))
 	}
 
-	// Create the command by joining the query parameters with & and adding them to the command string
+	// Create command variable with the command and queryParams
 	command := fmt.Sprintf(r.config.Command, strings.Join(queryParams, "&"))
 
-	// Retrieve the data from the RouterOS device using the Print function from the routerosv7_restfull_api package
-	data, err := pkg.Print(ctx, r.config.Host, r.config.Username, r.config.Password, command)
+	// Retrieve data from RouterOS device using the config values from the RouterOSDataRetriever instance
+	data, err := routerosv7_restfull_api.Print(ctx, r.config.Host, r.config.Username, r.config.Password, command)
+
+	// Check if there is an error
 	if err != nil {
 		return nil, err
 	}
 
-	// Return the data and nil error
+	// Return the data as interface{} and nil error
 	return data, nil
 }
 
-/*
-PrintJSON is a function to print the data as JSON.
-in this function i use interface{}
-After that, I loop through the response and convert each item to a map[string]interface{}.
-Finally, I marshal the data to JSON and print it to the console.
-*/
+// PrintJSON function to print the data as JSON to the console
 func PrintJSON(data interface{}) {
-	// Convert the data to a slice of interfaces
+
+	// Check if the data is not []interface{} type
 	response, ok := data.([]interface{})
 
-	// Check if the conversion was successful
+	// Print error message if the data is not []interface{} type and return from this function
 	if !ok {
 		fmt.Println("Failed to process data")
 		return
 	}
 
-	// Process and print the data as JSON by converting each item to a map[string]interface{}
-	var ipAddresses []map[string]interface{}
+	// Process and print the data as JSON
+	var resultJSON []map[string]interface{}
+
+	// Iterate over the response and append the data to resultJSON
 	for _, item := range response {
 		if ipItem, ok := item.(map[string]interface{}); ok {
-			ipAddresses = append(ipAddresses, ipItem)
+			resultJSON = append(resultJSON, ipItem)
 		}
 	}
 
-	// Marshal the data to JSON
+	// Marshal the resultJSON to JSON
 	jsonData, err := json.Marshal(map[string]interface{}{
-		"code":   200,
-		"status": "OK",
-		"data":   ipAddresses,
+		"code":   200,        // HTTP status code
+		"status": "OK",       // HTTP status message
+		"data":   resultJSON, // Data
 	})
+
+	// Print error message if there is an error and return from this function
 	if err != nil {
 		fmt.Println("Failed to marshal JSON:", err)
 		return
 	}
 
-	// Print the JSON
+	// Print the JSON string to the console
 	fmt.Println(string(jsonData))
 }
 
+// main function for this example application
 func main() {
 
-	// Set the parameters for the command to be executed
+	// Create params variable as map[string]string
 	params := map[string]string{
 		"address":  "192.168.88.1/24",
 		"disabled": "false",
 	}
 
-	/*
-		Create a new AppConfig with the default host, username, password, command, and params
-		In this example, the command is ip/address and the params are address=192.168.88.1/24 and disabled=false
-		if i use curl command, the command will be like this:
-		curl -k -u username:password "http://x.x.x.x/rest/ip/address?address=192.168.88.1/24&disabled=false" |jq
-	*/
+	// Create config variable with the default values and params
 	config := NewAppConfig(defaultHost, defaultUsername, defaultPassword, "ip/address?%s", params)
 
-	// Create a new RouterOSDataRetriever with the config
-	pingManager := pkg.NewPing(config.Host)
+	// Create a PingManager with host configuration for ping
+	pingManager := routerosv7_restfull_api.NewPing(config.Host)
 
-	// Check if the device is available
+	// Check if pingManager is nil
 	if pingManager == nil {
 		fmt.Println("Failed to create PingManager")
 		return
 	}
 
-	// Check if the device is available by calling the CheckAvailableDevice method
+	// Run pingManager and check if the device is available
 	err := pingManager.CheckAvailableDevice()
+
+	// Check if there is an error
 	if err != nil {
 		fmt.Println("Device is not available:", err)
 		return
@@ -163,8 +147,10 @@ func main() {
 
 	fmt.Println("Device is available")
 
-	// Authenticate to the RouterOS device by calling the AuthDevice method
-	err = pkg.AuthDevice(context.Background(), config.Host, config.Username, config.Password)
+	// Authenticate to the RouterOS device
+	err = routerosv7_restfull_api.AuthDevice(context.Background(), config.Host, config.Username, config.Password)
+
+	// Check if there is an error
 	if err != nil {
 		fmt.Println("Authentication failed:", err)
 		return
@@ -172,15 +158,16 @@ func main() {
 
 	fmt.Println("Authentication success")
 
-	// Create a new RouterOSDataRetriever with the config and call the GetData method to retrieve the data
+	// Create new RouterOSDataRetriever instance with the config values from the AppConfig instance
 	dataRetriever := NewRouterOSDataRetriever(config)
 
-	// Retrieve the data from the RouterOS device by calling the GetData method
+	// Retrieve data from RouterOS device using the config values from the RouterOSDataRetriever instance
 	data, err := dataRetriever.GetData(context.Background())
 	if err != nil {
 		fmt.Println("Failed to retrieve data:", err)
 		return
 	}
 
+	// Print the data as JSON to the console
 	PrintJSON(data)
 }
