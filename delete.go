aba5832fcb149396d4aaf4edd3cf8450e4d4779c
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-11-08 00:53:53 +0700

    add delete

--- delete.go
+++ delete.go
@@ -2,12 +2,11 @@ package routerosv7_restfull_api
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
-	"net/http"
 )
 
-func Delete(ctx context.Context, host, username, password, command string) error {
+// Delete is used to delete data from the router using the RESTful API.
+func Delete(ctx context.Context, host, username, password, command string) (interface{}, error) {
 
 	// Determine the protocol from the URL (HTTP or HTTPS)
 	protocol := determineProtocol(host)
@@ -15,42 +14,15 @@ func Delete(ctx context.Context, host, username, password, command string) error
 	// Create the URL for the request
 	url := fmt.Sprintf("%s://%s/rest/%s", protocol, host, command)
 
-	// Create a new HTTP request based on the provided URL and method
-	req, err := http.NewRequest("DELETE", url, nil)
-	if err != nil {
-		return err
+	// Create the request config
+	config := requestConfig{
+		URL:      url,      // URL for the request
+		Method:   "DELETE", // Method for the requests
+		Payload:  nil,      // Payload for the request
+		Username: username, // Username for the request
+		Password: password, // Password for the request
 	}
 
-	// Set basic auth for the request
-	req.SetBasicAuth(username, password)
-
-	// Set the content type to JSON
-	req.Header.Set("Content-Type", "application/json")
-
-	// Client for HTTP requests
-	client := &http.Client{}
-
-	// Set the TLS configuration for the client based on the protocol (HTTP or HTTPS)
-	if protocol == "https" {
-		// Use a client with standard TLS verification for HTTPS
-		client = &http.Client{}
-	} else {
-		// Use a client with insecure TLS configuration for HTTP
-		client = &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		}
-	}
-
-	req = req.WithContext(ctx)
-
-	// Send the request
-	_, err = client.Do(req)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+	// Make the request
+	return makeRequest(ctx, config)
 }
