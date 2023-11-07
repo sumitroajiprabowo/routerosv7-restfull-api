b1d62642fbdb0115bff76962f4437965c79354a3
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-11-07 21:14:14 +0700

    refactoring

--- auth.go
+++ auth.go
@@ -2,80 +2,32 @@ package routerosv7_restfull_api
 
 import (
 	"context"
-	"crypto/tls"
-	"errors"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"strings"
 )
 
-func AuthDevice(ctx context.Context, host, username, password string) error {
-	// Determine the protocol from the URL (HTTP or HTTPS) and check if the protocol is valid
-	// If the protocol is not valid, return error
-	protocol := determineProtocol(host)
+// AuthDeviceConfig is the configuration for the AuthDevice function
+type AuthDeviceConfig struct {
+	Host     string // Host for the request to Mikrotik Router
+	Username string // Username for the request to Mikrotik Router
+	Password string // Password for the request to Mikrotik Router
+}
+
+func AuthDevice(ctx context.Context, config AuthDeviceConfig) (interface{}, error) {
+	// Determine the protocol from the URL (HTTP or HTTPS)
+	protocol := determineProtocol(config.Host)
 
 	// Create the URL for the request to Mikrotik Router
-	url := fmt.Sprintf("%s://%s/rest/system/resource", protocol, host)
+	url := fmt.Sprintf("%s://%s/rest/system/resource", protocol, config.Host)
 
-	// Create request to Mikrotik Router based on "url" and "method" (GET)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
+	// Create the request configuration for the request to Mikrotik Router
+	requestConfig := requestConfig{
+		URL:      url,             // URL for the request to Mikrotik Router
+		Method:   "GET",           // Method for the request to Mikrotik Router
+		Payload:  nil,             // Payload for the request to Mikrotik Router
+		Username: config.Username, // Username for the request to Mikrotik Router
+		Password: config.Password, // Password for the request to Mikrotik Router
 	}
 
-	// Set basic auth for GET request to Mikrotik Router
-	req.SetBasicAuth(username, password)
-
-	// Create an HTTP client
-	var client *http.Client
-
-	// Check if protocol is https or http
-	if protocol == httpsProtocol {
-		// Attempt HTTPS, but if there is a TLS handshake error, fall back to HTTP
-		client = &http.Client{}
-	} else {
-		// Use a client with insecure TLS configuration for HTTP
-		client = &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		}
-	}
-
-	req = req.WithContext(ctx)
-
-	// Send the request
-	resp, err := client.Do(req)
-
-	if err != nil {
-		if strings.Contains(err.Error(), "tls: handshake failure") {
-			protocolWeb := httpProtocol
-			// TLS handshake failure, redirect to HTTP
-			url = fmt.Sprintf("%s://%s/rest/system/resource", protocolWeb, host)
-			req.URL, _ = req.URL.Parse(url)
-			resp, err = client.Do(req) // Retry with HTTP
-			if err != nil {
-				return err
-			}
-		} else {
-			return err
-		}
-	}
-
-	// Close response body
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Println(err)
-		}
-	}(resp.Body)
-
-	// Check if the response status code is 404 or 401 return exception.Unauthorized
-	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusUnauthorized {
-		return errors.New("unauthorized")
-	}
-
-	return nil
+	// Make the request to Mikrotik Router
+	return makeRequest(ctx, requestConfig)
 }
--- example/dial/dial.go
+++ example/dial/dial.go
@@ -3,17 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
-	pkg "github.com/megadata-dev/routerosv7-restfull-api"
+	"github.com/megadata-dev/routerosv7-restfull-api"
 )
 
+// Create a constant for the router's IP address, username, and password
 const (
-	routerIP = "10.90.0.251"
-	username = "userapi"
-	password = "password"
+	routerIP = "10.90.0.251" // Change this to your router's IP address
+	username = "userapi"     // Change this to your router's username
+	password = "password"    // Change this to your router's password
 )
 
+// The main function
 func main() {
+
+	// Authenticate to the router
 	err := authenticate(routerIP, username, password)
+
+	// Check if authentication failed
 	if err != nil {
 		fmt.Println("Authentication failed:", err)
 	} else {
@@ -21,6 +27,17 @@ func main() {
 	}
 }
 
+// authenticate authenticates to the router. If authentication fails, an error is returned.
 func authenticate(routerIP, username, password string) error {
-	return pkg.AuthDevice(context.Background(), routerIP, username, password)
+
+	// Create a config for the router to authenticate
+	config := routerosv7_restfull_api.AuthDeviceConfig{
+		Host:     routerIP, // Change this to your router's IP address
+		Username: username, // Change this to your router's username
+		Password: password, // Change this to your router's password
+	}
+
+	// Authenticate to the router
+	_, err := routerosv7_restfull_api.AuthDevice(context.Background(), config)
+	return err
 }
